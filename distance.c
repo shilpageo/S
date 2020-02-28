08a092b20ba320635839fa19c1f08b29fba03a40
shilpageo <shilpageo98@gmail.com>
2020-02-28 14:48:53 +0530

    Saved

---
+++ distance.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+float S,D,T;
+printf("enter the distance in KM");
+scanf("%f",&D);
+printf("\nenter time in Hr");
+scanf("%f",&T);
+S=D/T;
+printf("\nThe speed =%f",S);
+}
